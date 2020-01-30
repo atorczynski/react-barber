5e2980fe86290765e5e08643504cb2240af57bfc
atorczynski <artur.torcz@gmail.com>
2020-01-31 00:20:57 +0100

    Add Banner component to app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './components/Header';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import GlobalStyles from './GlobalStyles';
+import AppBanner from './components/AppBanner';
 
 function App() {
   return (
     <Router>
       <GlobalStyles />
       <Header />
+      <AppBanner />>
     </Router>
   );
 }
