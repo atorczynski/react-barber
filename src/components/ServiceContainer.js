3169b8f341ca4cd11146165b807be2bb9e6f807e
atorczynski <artur.torcz@gmail.com>
2020-02-17 04:24:26 +0100

    Add images to components and app.

--- src/components/ServiceContainer.js
+++ src/components/ServiceContainer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ServiceBox from './ServiceBox';
 import styled from '@emotion/styled';
+import Image1 from '../assets/barber-grooming-kid-2204690.jpg';
+import Image2 from '../assets/smiling-man-cutting-another-mans-hair-inside-shop-2061820.jpg';
+import Image3 from '../assets/a-barber-grooming-a-man-s-beard-2881253.jpg';
 
 const ServiceWrapper = styled.div`
   display: flex;
@@ -17,25 +20,19 @@ export default function ServiceContainer(props) {
       <ServiceBox
         serviceName={'Kids Haircut'}
         servicePrice={'$50'}
-        serviceTime={'1 Hour'}
-        serviceExtra1={'Premium Haircut'}
-        serviceExtra2={'Fulfill Kids Wish'}
+        serviceImage={Image1}
         onClick={props.onClick}
       ></ServiceBox>
       <ServiceBox
         serviceName={'Mister B'}
         servicePrice={'$125'}
-        serviceTime={'2 Hour'}
-        serviceExtra1={'Haircut and Beard'}
-        serviceExtra2={'Premium Aftershave'}
+        serviceImage={Image2}
         onClick={props.onClick}
       ></ServiceBox>
       <ServiceBox
         serviceName={'Kingsley'}
         servicePrice={'$175'}
-        serviceTime={'2 Hour'}
-        serviceExtra1={'Full Shave and Trimm'}
-        serviceExtra2={'Premium Styling'}
+        serviceImage={Image3}
         onClick={props.onClick}
       ></ServiceBox>
     </ServiceWrapper>
