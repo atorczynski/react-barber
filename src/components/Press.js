dd8da660fe811607bcde78a17e6dc20b618f6360
atorczynski <artur.torcz@gmail.com>
2020-02-19 02:54:50 +0100

    Add contact component

--- src/components/Press.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styled from '@emotion/styled';
-
-
-export default function Press() {
-  return <form></form>;
-}
---
+++ src/stories/6-Contact.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Contact from '../components/Contact';
+
+export default {
+  title: 'Contact'
+};
+
+export const ContactRender = () => <Contact></Contact>;
